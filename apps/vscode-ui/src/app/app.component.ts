795ef65723bf0aa1d691cbc86611b3af27ac1036
Rafał Wolak <r.wolak@design4.pro>
2020-01-23 20:08:34 +0100

    chore(package): initial commit

---
+++ apps/vscode-ui/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'vscode-webpack-monitor-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'vscode-ui';
+}
